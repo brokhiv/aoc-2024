36e1816c13e1872840ef822d83355def9bd243cf
Ivo Broekhof <46261484+brokhiv@users.noreply.github.com>
2024-12-07 15:28:34 +0100

    Day 7

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -1,11 +1,29 @@
 
 object Day07 extends Day {
   override val inputPath: String = "day07"
-  override type Puzzle = Nothing
+  override type Puzzle = List[(BigInt, List[BigInt])]
 
-  override def parsePuzzle: Day07.Parser[Puzzle] = ???
+  override def skipWhitespace: Boolean = false
+  private def number: Parser[BigInt] = """\d+""".r ^^ BigInt.apply
+  private def line: Parser[(BigInt, List[BigInt])] =
+    (number <~ ": ") ~ rep1sep(number, " ") ^^ { case t ~ ns => (t, ns) }
+  override def parsePuzzle: Day07.Parser[Puzzle] = rep1sep(line, "\n")
 
-  override def solve1(puzzle: Puzzle): Any = ???
+  override def solve1(puzzle: Puzzle): Any = {
+    puzzle.sumBy { (t, ns) =>
+      if ns.tail.foldLeft(Set(ns.head)) {
+        (cs, n) => cs.flatMap(c => List(c + n, c * n))
+      }.contains(t)
+      then t else BigInt(0)
+    }
+  }
 
-  override def solve2(puzzle: Puzzle): Any = ???
+  override def solve2(puzzle: Puzzle): Any = {
+    puzzle.sumBy { (t, ns) =>
+      if ns.tail.foldLeft(Set(ns.head)) {
+        (cs, n) => cs.flatMap(c => List(c + n, c * n, BigInt(c.toString + n.toString)))
+      }.contains(t)
+      then t else BigInt(0)
+    }
+  }
 }
